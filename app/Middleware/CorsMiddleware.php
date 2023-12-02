c97bb49ea0e7b460acc08929dcd55ce0078d7276
k99k5 <545551320@qq.com>
2023-12-02 20:23:57 +0800

    feat: cors中间件

---
+++ app/Middleware/CorsMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Hyperf\Context\Context;
+
+class CorsMiddleware implements MiddlewareInterface {
+	public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface {
+		$response = Context::get(ResponseInterface::class);
+		$response = $response->withHeader('Access-Control-Allow-Origin', '*')
+			->withHeader('Access-Control-Allow-Credentials', 'true')
+			// Headers 可以根据实际情况进行改写。
+			->withHeader('Access-Control-Allow-Headers', 'DNT,Keep-Alive,User-Agent,Cache-Control,Content-Type,Authorization');
+		
+		Context::set(ResponseInterface::class, $response);
+		
+		if ($request->getMethod() == 'OPTIONS') {
+			return $response;
+		}
+		
+		return $handler->handle($request);
+	}
+}
