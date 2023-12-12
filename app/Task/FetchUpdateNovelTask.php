0938b06ae746f43e78cfcd79f1c46411bf264182
k99k5 <k99k5@qq.com>
2023-12-12 16:48:57 +0800

    fix: 小说现在每8小时更新一次

--- app/Task/FetchUpdateNovelTask.php
+++ app/Task/FetchUpdateNovelTask.php
@@ -13,14 +13,14 @@ use Hyperf\Database\Model\Collection;
 use Hyperf\Database\Model\Builder;
 use Hyperf\Di\Annotation\Inject;
 
-#[Crontab(rule: "0 0 4,12,20 * *", name: "FetchUpdateNovelTask", callback: "execute", memo: "采集小说任务")]
+#[Crontab(rule: "0 4,12,16,20 * * *", name: "FetchUpdateNovelTask", callback: "execute", memo: "采集小说任务")]
 class FetchUpdateNovelTask {
 	#[Inject]
 	protected FetchQueueService $fetchQueueService;
 	
 	public function execute(): void {
 		Novel::where(function (Builder $query) {
-			$query->where('fetched_at', '>', time() - 3600 * 8);
+			$query->where('fetched_at', '<', Carbon::now()->subHours(8));
 		})->chunkById(10, function (Collection $novels) {
 			$novels->each(function (Novel $novel) {
 				$rule = FetchRule::getRule($novel->source);
