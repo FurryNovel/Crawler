6bb4865752aaeab4991a2ad9cf5d1baa37a2fb39
k99k5 <k99k5@qq.com>
2023-12-20 17:23:45 +0800

    feat: 采集15页

--- app/Task/FetchLatestNovelTask.php
+++ app/Task/FetchLatestNovelTask.php
@@ -31,7 +31,7 @@ class FetchLatestNovelTask {
 				$max_page = PHP_INT_MAX;
 				$last_id = $cache->get(sprintf('fetch_latest_%s_%s', $rule->getType(), $tag), null);
 				if (!$last_id) {
-					$max_page = 5;
+					$max_page = 15;
 				}
 				$is_first = true;
 				while ($page < $max_page) {
