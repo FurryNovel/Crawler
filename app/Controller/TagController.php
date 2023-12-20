9ca2022143361164c4bc4f4e52883910900cc8a6
k99k5 <k99k5@qq.com>
2023-12-20 16:59:40 +0800

    feat: 缓存

--- app/Controller/TagController.php
+++ app/Controller/TagController.php
@@ -4,24 +4,37 @@ declare(strict_types = 1);
 namespace App\Controller;
 
 use App\Controller\Abstract\FS_Controller;
+use App\DataSet\DataSet;
 use App\Model\Chapter;
 use App\Model\Novel;
 use App\Model\Tag;
+use App\Utils\Utils;
 use Hyperf\Cache\Annotation\Cacheable;
 use Hyperf\Database\Model\Builder;
 use Hyperf\Database\Query\JoinClause;
+use Hyperf\Di\Annotation\Inject;
 use Hyperf\HttpServer\Annotation\Controller;
 use Hyperf\HttpServer\Annotation\RequestMapping;
 
 #[Controller]
 class TagController extends FS_Controller {
+	#[Inject]
+	protected DataSet $dataSet;
+	
 	protected function baseQuery(): Builder {
 		return Tag::where('count', '>', 0)->orderBy('count', 'desc');
 	}
 	
-	#[Cacheable(prefix: __CLASS__, ttl: 3600)]
 	#[RequestMapping(path: '', methods: 'get')]
 	function index(): array {
-		return $this->success($this->baseQuery()->select()->get(), '获取成功');
+		return $this->success($this->tags(Utils::getVisitorLanguage()), '获取成功');
+	}
+	
+	
+	#[Cacheable(prefix: __CLASS__, value: '_#{lang}', ttl: 3600)]
+	private function tags($lang): array {
+		return $this->baseQuery()->select()->get()->each(function (Tag $tag) use ($lang) {
+			$tag->name = $this->dataSet->convertTo($lang, null, [$tag->name])[0] ?? $tag->name;
+		})->toArray();
 	}
 }
--- app/Middleware/CorsMiddleware.php
+++ app/Middleware/CorsMiddleware.php
@@ -13,8 +13,8 @@ class CorsMiddleware implements MiddlewareInterface {
 		$response = Context::get(ResponseInterface::class);
 		$response = $response->withHeader('Access-Control-Allow-Origin', '*')
 			->withHeader('Access-Control-Allow-Credentials', 'true')
-			// Headers 可以根据实际情况进行改写。
-			->withHeader('Access-Control-Allow-Headers', 'DNT,Keep-Alive,User-Agent,Cache-Control,Content-Type,Authorization');
+			->withHeader('Access-Control-Allow-Headers', 'DNT,Keep-Alive,User-Agent,Cache-Control,Content-Type,Authorization')
+			->withHeader('Access-Control-Max-Age', 3600);
 		
 		Context::set(ResponseInterface::class, $response);
 		
