90b3ae482895433b3c73b83c00fea9191239edc1
k99k5 <k99k5@qq.com>
2024-01-04 16:20:40 +0800

    feat: 超时自动重试队列

--- config/autoload/listeners.php
+++ config/autoload/listeners.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 /**
  * This file is part of Hyperf.
  *
@@ -10,7 +10,8 @@ declare(strict_types=1);
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
 return [
-    Hyperf\ExceptionHandler\Listener\ErrorExceptionHandler::class,
-    Hyperf\Command\Listener\FailToHandleListener::class,
+	Hyperf\ExceptionHandler\Listener\ErrorExceptionHandler::class,
+	Hyperf\Command\Listener\FailToHandleListener::class,
 	Hyperf\ModelCache\Listener\EagerLoadListener::class,
+	Hyperf\AsyncQueue\Listener\ReloadChannelListener::class,
 ];
