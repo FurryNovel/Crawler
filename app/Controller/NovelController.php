2639d30f47386d81fa195dd245113dcf5d6adb1f
k99k5 <545551320@qq.com>
2024-01-05 00:48:22 +0800

    feat: 支持多重tag

--- app/Controller/NovelController.php
+++ app/Controller/NovelController.php
@@ -24,34 +24,55 @@ class NovelController extends FS_Controller {
 		$query = Novel::where('novel.status', Novel::STATUS_PUBLISH);
 		
 		$ids = $this->request->input('ids');
-		$tag = Utils::filterLike($this->request->input('tag'));
+		$tags = array_values(array_filter($this->request->input('tags', []), 'App\Utils\Utils::filterLike'));
+		if (empty($tags)) {
+			$tags = Utils::filterLike($this->request->input('tag'));
+			if (!empty($tags) and is_string($tags)) {
+				$tags = [$tags];
+			}
+		}
 		$hate_tags = array_values(array_filter($this->request->input('hate_tags', []), 'App\Utils\Utils::filterLike'));
 		$keyword = Utils::filterLike($this->request->input('keyword'));
 		$user_id = intval($this->request->input('user_id'));
 		$order_by = $this->request->input('order_by', 'latest');
 		$order = $this->request->input('order', 'desc');
 		
+		
 		switch ($order) {
 			case 'desc':
 			case 'asc':
 				break;
-			
 			default:
 				$order = 'desc';
 				break;
 		}
 		
-		if (!empty($tag)) {
-			$query->where(function (Builder $query) use ($tag) {
-				$tag = $this->dataSet->convertToPattern(null, [$tag])[0] ?? '';
-				$query->where('novel.tags', 'like', '%' . $tag . '%');
-				$query->whereIn('novel.id', function (\Hyperf\Database\Query\Builder $query) use ($tag) {
-					$query->select('novel_id')
-						->from('chapter')
-						->where('chapter.status', Chapter::STATUS_PUBLISH)
-						->where('chapter.tags', 'like', '%' . $tag . '%');
-				}, 'OR');
-			});
+		if (!empty($tags)) {
+			if (count($tags) == 1) {
+				$query->where(function (Builder $query) use ($tags) {
+					$tag = $this->dataSet->convertToPattern(null, $tags)[0] ?? '';
+					$query->where('novel.tags', 'like', '%' . $tag . '%');
+					$query->whereIn('novel.id', function (\Hyperf\Database\Query\Builder $query) use ($tag) {
+						$query->select('novel_id')
+							->from('chapter')
+							->where('chapter.status', Chapter::STATUS_PUBLISH)
+							->where('chapter.tags', 'like', '%' . $tag . '%');
+					}, 'OR');
+				});
+			} else {
+				$tags = array_values(
+					array_slice(
+						$this->dataSet->convertToPattern(null, $tags),
+						0,
+						10
+					)
+				);
+				if (!empty($tags)) {
+					foreach ($tags as $tag) {
+						$query->where('novel.tags', 'like', '%' . $tag . '%');
+					}
+				}
+			}
 		}
 		if (!empty($keyword)) {
 			$query->where(function (Builder $query) use ($keyword) {
