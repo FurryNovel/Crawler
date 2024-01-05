59d02dd31738e2c6e97e9e3f1954b7f77e47dba7
k99k5 <545551320@qq.com>
2024-01-05 21:36:21 +0800

    fix: 语言识别库分层检测

--- app/DataSet/LanguageService.php
+++ app/DataSet/LanguageService.php
@@ -27,11 +27,6 @@ class LanguageService {
 	
 	function detect(string $text): string {
 		$check = max(mb_strlen($text) / 2, 1);
-		// 检查是否包含中文
-		if (preg_match_all('/[\x{4E00}-\x{9FBF}]/u', $text) >= $check) {
-			//检查是否包含繁体中文
-			return $this->detectDeep($text, ['zh-cn' => 2, 'zh-tw' => 2.5]);
-		}
 		// 检查是否包含日文
 		if (preg_match_all('/[\x{3040}-\x{309F}\x{30A0}-\x{30FF}\x{31F0}-\x{31FF}\x{FF65}-\x{FF9F}]/u', $text) >= $check) {
 			return 'ja';
@@ -39,6 +34,11 @@ class LanguageService {
 		// 检查是否包含韩文
 		if (preg_match_all('/[\x{1100}-\x{11FF}\x{3130}-\x{318F}\x{AC00}-\x{D7AF}]/u', $text) >= $check) {
 			return 'ko';
+		}
+		// 检查是否包含中文
+		if (preg_match_all('/[\x{4E00}-\x{9FBF}]/u', $text) >= $check) {
+			//检查是否包含繁体中文
+			return $this->detectDeep($text, ['zh-cn' => 2, 'zh-tw' => 2.5]);
 		}
 		return $this->detectDeep($text);
 	}
