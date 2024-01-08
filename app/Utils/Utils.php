93c93b86e4ea547064b34d9b097c6632ddbae8f1
k99k5 <k99k5@qq.com>
2024-01-08 17:27:46 +0800

    fix: 采集时队列出错

--- app/Utils/Utils.php
+++ app/Utils/Utils.php
@@ -9,19 +9,23 @@ class Utils {
 	const DEFAULT_LANGUAGE = 'zh_CN';
 	
 	static public function getVisitorIP() {
-		$container = ApplicationContext::getContainer();
-		if ($container->has(RequestInterface::class)) {
-			$request = ApplicationContext::getContainer()->get(RequestInterface::class);
-			$res = $request->getServerParams();
-			if (isset($res['http_client_ip'])) {
-				return $res['http_client_ip'];
-			} elseif (isset($res['http_x_real_ip'])) {
-				return $res['http_x_real_ip'];
-			} elseif (isset($res['http_x_forwarded_for'])) {
-				$arr = explode(',', $res['http_x_forwarded_for']);
-				return $arr[0];
+		if (ApplicationContext::hasContainer()) {
+			$container = ApplicationContext::getContainer();
+			if ($container->has(RequestInterface::class)) {
+				$request = ApplicationContext::getContainer()->get(RequestInterface::class);
+				$res = $request->getServerParams();
+				if (isset($res['http_client_ip'])) {
+					return $res['http_client_ip'];
+				} elseif (isset($res['http_x_real_ip'])) {
+					return $res['http_x_real_ip'];
+				} elseif (isset($res['http_x_forwarded_for'])) {
+					$arr = explode(',', $res['http_x_forwarded_for']);
+					return $arr[0];
+				} else {
+					return $res['remote_addr'];
+				}
 			} else {
-				return $res['remote_addr'];
+				return '';
 			}
 		} else {
 			return '';
@@ -29,16 +33,20 @@ class Utils {
 	}
 	
 	static public function getVisitorLanguage(): string {
-		$container = ApplicationContext::getContainer();
-		if ($container->has(RequestInterface::class)) {
-			$request = $container->get(RequestInterface::class);
-			$res = $request->getHeader('accept-language');
-			if (isset($res[0])) {
-				$arr = explode(',', $res[0]);
-				if (isset($arr[0])) {
-					$arr[0] = str_replace('-', '_', $arr[0]);
+		if (ApplicationContext::hasContainer()) {
+			$container = ApplicationContext::getContainer();
+			if ($container->has(RequestInterface::class)) {
+				$request = $container->get(RequestInterface::class);
+				$res = $request->getHeader('accept-language');
+				if (isset($res[0])) {
+					$arr = explode(',', $res[0]);
+					if (isset($arr[0])) {
+						$arr[0] = str_replace('-', '_', $arr[0]);
+					}
+					return $arr[0] ?? self::DEFAULT_LANGUAGE;
+				} else {
+					return self::DEFAULT_LANGUAGE;
 				}
-				return $arr[0] ?? self::DEFAULT_LANGUAGE;
 			} else {
 				return self::DEFAULT_LANGUAGE;
 			}
