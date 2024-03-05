33187ab5b63e0310902dbf81f6b99aaf7c42b6be
k99k5 <k99k5@qq.com>
2024-03-05 17:18:49 +0800

    🐛 fix: allow CORS from any host in CorsMiddleware.php

--- app/Middleware/CorsMiddleware.php
+++ app/Middleware/CorsMiddleware.php
@@ -11,11 +11,17 @@ use Hyperf\Context\Context;
 class CorsMiddleware implements MiddlewareInterface {
 	public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface {
 		$response = Context::get(ResponseInterface::class);
-		$host = 'https://novel.tigerkk.me';
-		if ($request->hasHeader('host')) {
-			$host = $request->getHeaderLine('host');
+		
+		if ($request->hasHeader('Origin')) {
+			$origin = $request->getHeaderLine('Origin');
+			if (!str_contains($origin, 'tigerkk.me') and !str_contains($origin, 'furrynovel.com')) {
+				$origin = 'https://novel.tigerkk.me';
+			}
+		} else {
+			$origin = 'https://novel.tigerkk.me';
 		}
-		$response = $response->withHeader('Access-Control-Allow-Origin', 'https://' . $host)
+		
+		$response = $response->withHeader('Access-Control-Allow-Origin', $origin)
 			->withHeader('Access-Control-Allow-Credentials', 'true')
 			->withHeader('Access-Control-Allow-Headers', 'DNT,Keep-Alive,User-Agent,Cache-Control,Content-Type,Authorization')
 			->withHeader('Access-Control-Max-Age', 3600);
